d01876dd2b3a127c80eb6b93b58ce500cc135442
iaiapps <ikromudin.ai@gmail.com>
2023-09-13 13:39:13 +0700

    fix link

--- config/filesystems.php
+++ config/filesystems.php
@@ -69,13 +69,8 @@ return [
     |
     */
 
-    // 'links' => [
-    //     public_path('storage') => storage_path('app/public'),
-    // ],
-
-    //hostinger
     'links' => [
-        base_path('public_html/storage') => storage_path('app/public'),
+        public_path('storage') => storage_path('app/public'),
     ],
 
 ];
