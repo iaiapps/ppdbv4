d9d7197cde5f1419413b9c727509aca10106e2b2
iaiapps <ikromudin.ai@gmail.com>
2023-09-20 21:44:38 +0700

    fix costCategory

--- resources/views/admin/setting/costcat/index.blade.php
+++ resources/views/admin/setting/costcat/index.blade.php
@@ -15,9 +15,16 @@
             <table id="table" class="table table-bordered rounded align-middle">
                 <thead>
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Name</th>
+                        {{-- <th scope="col">#</th> --}}
+                        <th scope="col" class="fs-small">Name</th>
                         <th scope="col">Gender</th>
+                        <th scope="col">Gedung</th>
+                        <th scope="col">Perpustakaan</th>
+                        <th scope="col">Kegiatan</th>
+                        <th scope="col">Buku_Media</th>
+                        <th scope="col">Seragam</th>
+                        <th scope="col">Jilbab</th>
+                        <th scope="col">Ipp</th>
                         <th scope="col">Total</th>
                         <th scope="col">Action</th>
                     </tr>
@@ -25,22 +32,29 @@
                 <tbody>
                     @foreach ($costcats as $costcat)
                         <tr>
-                            <td>{{ $loop->iteration }}</td>
+                            {{-- <td>{{ $loop->iteration }}</td> --}}
                             <td>{{ $costcat->name }}</td>
                             <td>{{ $costcat->gender }}</td>
+                            <td>{{ $costcat->gedung }}</td>
+                            <td>{{ $costcat->perpustakaan }}</td>
+                            <td>{{ $costcat->kegiatan }}</td>
+                            <td>{{ $costcat->bukumedia }}</td>
+                            <td>{{ $costcat->seragam }}</td>
+                            <td>{{ $costcat->jilbab }} </td>
+                            <td>{{ $costcat->ipp }}</td>
                             <td>Rp.
                                 {{ $costcat->gedung + $costcat->perpustakaan + $costcat->kegiatan + $costcat->bukumedia + $costcat->seragam + $costcat->jilbab + $costcat->ipp }}
                             </td>
                             <td>
                                 <a href="{{ route('costCategory.edit', $costcat->id) }}" class="btn btn-orange btn-sm"><i
-                                        class="bi bi-pencil-square"></i> edit</a>
+                                        class="bi bi-pencil-square"></i></a>
                                 <form onsubmit="return confirm('Apakah anda yakin untuk menghapus data ?');"
                                     action="{{ route('costCategory.destroy', $costcat->id) }}" method="post"
                                     class="d-inline">
                                     @csrf
                                     @method('delete')
                                     <button type="submit" class="btn btn-danger btn-sm">
-                                        <i class="bi bi-trash3"></i> del
+                                        <i class="bi bi-trash3"></i>
                                     </button>
                                 </form>
                             </td>
@@ -53,6 +67,14 @@
     </div>
 @endsection
 @include('layouts.partial.scripts')
+
+@push('css')
+    <style>
+        #table {
+            font-size: 14px
+        }
+    </style>
+@endpush
 
 @push('scripts')
     <script>
