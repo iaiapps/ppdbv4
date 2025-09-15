aa6103f5ea70a70197edbf297778bac208daf5af
iaiapps <ikromudin.ai@gmail.com>
2025-09-15 16:53:09 +0700

    fix minor form

--- resources/views/student/form.blade.php
+++ resources/views/student/form.blade.php
@@ -74,7 +74,7 @@
                                 </div>
                                 <div class="col-12 col-md-6">
                                     <div class="mb-3">
-                                        <label class="form-label" for="kk">No KK</label>
+                                        <label class="form-label" for="kk">Nomor KK</label>
                                         <input class="form-control" id="kk" name="kk" type="text" required />
                                     </div>
                                 </div>
